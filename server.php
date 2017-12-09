8b01263fc164dbd4899a8c4922bf997c261bd6d0
matsisc <marco.tapia@outlook.com>
2017-12-09 11:24:46 -0600

    Corregido Autor

--- server.php
+++ server.php
@@ -4,7 +4,7 @@
  * Laravel - A PHP Framework For Web Artisans
  *
  * @package  Laravel
- * @author   Taylor Otwell <taylor@laravel.com>
+ * @author   Marco Tapia <Marco@laravel.com>
  */
 
 $uri = urldecode(
